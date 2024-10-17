76c886f5325cf359eb91d1f9351bc8735125c6bb
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-10-17 23:19:55 +0000

    setup

--- server.py
+++ server.py
@@ -11,7 +11,7 @@ app = FastAPI()
 
 app.add_middleware(
   CORSMiddleware,
-  allow_origins=["*"],  # Adjust this to be more specific in production
+  allow_origins=["*"],
   allow_credentials=True,
   allow_methods=["*"],
   allow_headers=["*"],
